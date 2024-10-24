68a3158fd7a6ed5fda1a3895a2e601833bafe798
ConanisaCoder <87452767+ConanisaCoder@users.noreply.github.com>
2024-10-24 19:04:45 -0400

    Add files via upload

---
+++ Madlibs_game.py
@@ -0,0 +1,21 @@
+#word game where you create a story
+#by filling in blanks with random words 
+adjective1 = input("Input an adjective: ")
+animal = input("Input an mythical animal: ")
+heigth_cm = float(input("Heigth in Centimeters: "))
+heigth_m = heigth_cm / 100
+verb = input("Input a verb: ")
+adverb = input("Input a adverb: ")
+workplace = input(" Input a Workplace: ")
+occupation = input(" Input a Job :")
+event = input("Input an event:")
+
+
+print (f"I woke up in a very {adjective1} mood.") 
+print(f"When I looked out the window I saw a {animal} that was a weird ")  
+print(f"I mesured my heigth to {heigth_cm}")  
+print(f"I then out went to {verb} {adverb}  ")
+print(f"I went my workplace which is {workplace} I work as a {occupation}")
+print(f"After my work I came back home")
+print(f"But this was not the end of it as a {event}") 
+print(f"It was a dream or was it... ")       
